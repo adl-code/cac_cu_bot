1deb3f633c428545ae934b91bf7caa84ec13e43a
ducle_dev <ducle_dev@localhost.com>
2017-04-22 02:49:38 +0700

    Update scrpits

--- bot/RunMe.sh
+++ bot/RunMe.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source `which virtualenvwrapper.sh`
 workon slackbot
+PID_FILE=cac_cu_bot.pid
+if [ -f $PID_FILE ]; then
+	kill -9 $(cat $PID_FILE)
+fi
 nohup python BotRunner.py --config=conf/bot.conf </dev/null >/dev/null 2>&1 &
